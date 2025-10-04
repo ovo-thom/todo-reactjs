13adf744cd180525d00e58c220f1caab5e744767
Thomas OvO <thonnard.thomas@gmail.com>
2025-10-04 15:14:27 +0200

    feat: add newTodo: Todo initial object and state todos

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,19 @@ type Todo = {
 function App() {
   const [input, setInput] = useState<string>("");
   const [priority, setPriority] = useState<Priority>("Moyenne");
+  const [todos, setTodos = useState<Todo[]>([])];
+
+  function addTodo() {
+    if (input.trim() == "") {
+      return;
+    }
+
+    const newTodo: Todo = {
+      id: Date.now(),
+      text: input.trim(),
+      priority: priority,
+    };
+  }
   return (
     <div className="flex justify-center">
       <div className="w-2/3 flex flex-col gap-4 my-15 bg-base-300 p-5 rounded-2xl">
@@ -31,7 +44,9 @@ function App() {
             <option value="Moyenne">Moyenne</option>
             <option value="Basse">Basse</option>
           </select>
-          <button className="btn btn-primary">Ajouter</button>
+          <button onClick={addTodo} className="btn btn-primary">
+            Ajouter
+          </button>
         </div>
       </div>
     </div>
