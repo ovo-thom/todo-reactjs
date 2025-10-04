a3307ad80523447aa2bd61348bf047aea5a745c1
Thomas OvO <thonnard.thomas@gmail.com>
2025-10-04 14:45:49 +0200

    feat: add UI and input, select, button and add type for Todo and Priority (Typescript)

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,41 @@
-type Priority = "Urgente" | "Moyenne" | "Basse"
+import { useState } from "react";
+
+type Priority = "Urgente" | "Moyenne" | "Basse";
 
 type Todo = {
   id: number;
   text: string;
-  priority : Priority
-}
+  priority: Priority;
+};
 
 function App() {
-
+  const [input, setInput] = useState<string>("");
+  const [priority, setPriority] = useState<Priority>("Moyenne");
   return (
-    <div className="">
-    <button className="btn btn-accent">test</button>
+    <div className="flex justify-center">
+      <div className="w-2/3 flex flex-col gap-4 my-15 bg-base-300 p-5 rounded-2xl">
+        <div className="flex gap-4">
+          <input
+            type="text"
+            className="input w-full"
+            placeholder="Ajouter une tâche"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+          />
+          <select
+            className="select w-full"
+            value={priority}
+            onChange={(e) => setPriority(e.target.value as Priority)}
+          >
+            <option value="Urgente">Urgente</option>
+            <option value="Moyenne">Moyenne</option>
+            <option value="Basse">Basse</option>
+          </select>
+          <button className="btn btn-primary">Ajouter</button>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
