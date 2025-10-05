98edf417a261d8ed5248cdf94b845c62f8a174d7
Thomas OvO <thonnard.thomas@gmail.com>
2025-10-05 19:21:45 +0200

    feat: add todos into localStorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 type Priority = "Urgente" | "Moyenne" | "Basse";
 
@@ -11,7 +11,14 @@ type Todo = {
 function App() {
   const [input, setInput] = useState<string>("");
   const [priority, setPriority] = useState<Priority>("Moyenne");
-  const [todos, setTodos = useState<Todo[]>([])];
+
+  const savedTodos = localStorage.getItem("todos");
+  const initialTodos = savedTodos ? JSON.parse(savedTodos) : [];
+  const [todos, setTodos] = useState<Todo[]>(initialTodos);
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
 
   function addTodo() {
     if (input.trim() == "") {
@@ -23,7 +30,14 @@ function App() {
       text: input.trim(),
       priority: priority,
     };
+
+    const newTodos = [newTodo, ...todos];
+    setTodos(newTodos);
+    setInput("");
+    setPriority("Moyenne");
+    // console.log(newTodos);
   }
+
   return (
     <div className="flex justify-center">
       <div className="w-2/3 flex flex-col gap-4 my-15 bg-base-300 p-5 rounded-2xl">
