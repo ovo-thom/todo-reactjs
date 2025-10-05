fb5f5ebb4b5e2dcd0af04252991ea4f0372ddb22
Thomas OvO <thonnard.thomas@gmail.com>
2025-10-05 21:44:17 +0200

    feat: add priority filter and style li and Badge color

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import TodoItem from "./TodoItem";
 
 type Priority = "Urgente" | "Moyenne" | "Basse";
 
@@ -15,6 +16,7 @@ function App() {
   const savedTodos = localStorage.getItem("todos");
   const initialTodos = savedTodos ? JSON.parse(savedTodos) : [];
   const [todos, setTodos] = useState<Todo[]>(initialTodos);
+  const [filter, setFilter] = useState<Priority | "Tous">("Tous");
 
   useEffect(() => {
     localStorage.setItem("todos", JSON.stringify(todos));
@@ -38,6 +40,18 @@ function App() {
     // console.log(newTodos);
   }
 
+  let filteredTodos: Todo[] = [];
+
+  if (filter === "Tous") {
+    filteredTodos = todos;
+  } else {
+    filteredTodos = todos.filter((todo) => todo.priority === filter);
+  }
+
+  const urgentCount = todos.filter((t) => t.priority === "Urgente").length;
+  const mediumCount = todos.filter((t) => t.priority === "Moyenne").length;
+  const lowtCount = todos.filter((t) => t.priority === "Basse").length;
+  const totalCount = todos.length;
   return (
     <div className="flex justify-center">
       <div className="w-2/3 flex flex-col gap-4 my-15 bg-base-300 p-5 rounded-2xl">
@@ -61,6 +75,30 @@ function App() {
           <button onClick={addTodo} className="btn btn-primary">
             Ajouter
           </button>
+        </div>
+
+        <div className="space-y-2 flex-1 h-fit">
+          <div className="flex flex-wrap gap-4">
+            <button
+              className={`btn btn-soft ${
+                filter === "Tous" ? "btn-primary" : ""
+              }`}
+              onClick={() => setFilter("Tous")}
+            >
+              Tous ({totalCount})
+            </button>
+          </div>
+          {filteredTodos.length > 0 ? (
+            <ul className="divide-y divide-primary/20">
+              {filteredTodos.map((todo) => (
+                <li key={todo.id}>
+                  <TodoItem todo={todo} />
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <div>test2</div>
+          )}
         </div>
       </div>
     </div>
